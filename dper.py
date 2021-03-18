1958e86db33d74d0d7fe05a23130ec2b35fe1224
Jakob Schlyter <jakob@kirei.se>
2021-03-18 08:10:32 +0100

    reformat

--- dper.py
+++ dper.py
@@ -49,30 +49,42 @@ import voluptuous.humanize
 import yaml
 
 IP_ADDRESS = ipaddress.ip_address
-DOMAIN_NAME = vol.Any(vol.Match(r'\w+'), vol.Match(vol.DOMAIN_REGEX))
+DOMAIN_NAME = vol.Any(vol.Match(r"\w+"), vol.Match(vol.DOMAIN_REGEX))
 
-CONFIG_SCHEMA = vol.Schema({
-    vol.Required('output_format'): vol.Any('nsd'),
-    vol.Required('output_file'): str,
-    'output_diff': bool,
-    'cache_dir': vol.IsDir,
-    'zonefiles': bool,
-    vol.Required('peers'): vol.Schema({
-        str: vol.Schema({
-            vol.Required('source'): vol.FqdnUrl,
-            vol.Required('format'): vol.Any('xml', 'json'),
-        }),
-    }),
-    'reconfigure_command': str,
-})
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required("output_format"): vol.Any("nsd"),
+        vol.Required("output_file"): str,
+        "output_diff": bool,
+        "cache_dir": vol.IsDir,
+        "zonefiles": bool,
+        vol.Required("peers"): vol.Schema(
+            {
+                str: vol.Schema(
+                    {
+                        vol.Required("source"): vol.FqdnUrl,
+                        vol.Required("format"): vol.Any("xml", "json"),
+                    }
+                ),
+            }
+        ),
+        "reconfigure_command": str,
+    }
+)
 
-DYNAMIC_CONFIG_SCHEMA = vol.Schema({
-    vol.Required('masters'): vol.Schema([{
-        vol.Required('ip'): IP_ADDRESS,
-        vol.Required('tsig'): DOMAIN_NAME,
-    }]),
-    vol.Required('zones'): vol.Schema([DOMAIN_NAME])
-})
+DYNAMIC_CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required("masters"): vol.Schema(
+            [
+                {
+                    vol.Required("ip"): IP_ADDRESS,
+                    vol.Required("tsig"): DOMAIN_NAME,
+                }
+            ]
+        ),
+        vol.Required("zones"): vol.Schema([DOMAIN_NAME]),
+    }
+)
 
 REQUESTS_TIMEOUT = (5, 30)
 
@@ -94,8 +106,10 @@ class Peer(object):
 
 def parse_dynamic_config_dict(peer_id: str, config: dict) -> Peer:
     voluptuous.humanize.validate_with_humanized_errors(config, DYNAMIC_CONFIG_SCHEMA)
-    masters = [PeerMaster(ip=master['ip'], tsig=master['tsig']) for master in config['masters']]
-    zones = [re.sub(r'\.$', '', z) for z in config['zones']]
+    masters = [
+        PeerMaster(ip=master["ip"], tsig=master["tsig"]) for master in config["masters"]
+    ]
+    zones = [re.sub(r"\.$", "", z) for z in config["zones"]]
     logger.debug("Dynamic config for peer %s OK, %d zones", peer_id, len(zones))
     return Peer(id=peer_id, masters=masters, zones=zones)
 
@@ -110,22 +124,16 @@ def parse_dynamic_config_xml(peer_id: str, data: str) -> List[Peer]:
     logger.debug("Reading dynamic config for peer %s as XML", peer_id)
     peers = []
     xml_root = ET.fromstring(data)
-    for peer in xml_root.findall('./peer'):
+    for peer in xml_root.findall("./peer"):
         masters = []
         zones = []
-        name = peer.attrib['name']
-        for p in peer.findall('./primary'):
-            masters.append({
-                'ip': p.text,
-                'tsig': p.attrib['tsig']
-            })
-        for z in peer.findall('./zone'):
+        name = peer.attrib["name"]
+        for p in peer.findall("./primary"):
+            masters.append({"ip": p.text, "tsig": p.attrib["tsig"]})
+        for z in peer.findall("./zone"):
             zones.append(z.text)
-        config = {
-            'masters': masters,
-            'zones': zones
-        }
-        peers.append(parse_dynamic_config_dict(peer_id + '/' + name, config))
+        config = {"masters": masters, "zones": zones}
+        peers.append(parse_dynamic_config_dict(peer_id + "/" + name, config))
     return peers
 
 
@@ -135,7 +143,9 @@ def check_peers(peers: List[Peer]):
     for peer in peers:
         for zone in peer.zones:
             if zone in all_zones:
-                logger.critical("zone %s defined by both %s and %s", zone, all_zones[zone], peer.id)
+                logger.critical(
+                    "zone %s defined by both %s and %s", zone, all_zones[zone], peer.id
+                )
                 zone_errors += 1
             else:
                 all_zones[zone] = peer.id
@@ -144,7 +154,7 @@ def check_peers(peers: List[Peer]):
 
 
 def zone2file(zone: str) -> str:
-    return re.sub(r'/', '-', zone.lower())
+    return re.sub(r"/", "-", zone.lower())
 
 
 def generate_nsd(peers: List[Peer], use_zonefiles: bool = False):
@@ -162,10 +172,12 @@ def generate_nsd(peers: List[Peer], use_zonefiles: bool = False):
             print("")
 
 
-def get_unless_modified(url: str, modified: Optional[datetime]) -> Optional[requests.Response]:
+def get_unless_modified(
+    url: str, modified: Optional[datetime]
+) -> Optional[requests.Response]:
     headers = {}
     if modified is not None:
-        headers['If-Modified-Since'] = modified.strftime('%a, %d %b %Y %H:%M:%S GMT')
+        headers["If-Modified-Since"] = modified.strftime("%a, %d %b %Y %H:%M:%S GMT")
     response = requests.get(url, headers=headers, timeout=REQUESTS_TIMEOUT)
     logger.debug("GET %s returned %d", url, response.status_code)
     if response.status_code == 304:
@@ -174,7 +186,13 @@ def get_unless_modified(url: str, modified: Optional[datetime]) -> Optional[requ
     return response
 
 
-def process_dper(peer_id: str, source: str, cache: Optional[str], force_cache: bool, payload_format: str) -> List[Peer]:
+def process_dper(
+    peer_id: str,
+    source: str,
+    cache: Optional[str],
+    force_cache: bool,
+    payload_format: str,
+) -> List[Peer]:
     if cache is not None:
         modified: Optional[datetime] = None
         try:
@@ -185,45 +203,47 @@ def process_dper(peer_id: str, source: str, cache: Optional[str], force_cache: b
         response = None if force_cache else get_unless_modified(source, modified)
 
         if response is None:
-            with open(cache, 'rt') as cache_file:
+            with open(cache, "rt") as cache_file:
                 payload = cache_file.read()
         else:
             response.raise_for_status()
             payload = response.text
-            with open(cache, 'wt') as cache_file:
+            with open(cache, "wt") as cache_file:
                 cache_file.write(payload)
     else:
         response = requests.get(source, timeout=REQUESTS_TIMEOUT)
         response.raise_for_status()
         payload = response.text
 
-    if payload_format == 'json':
+    if payload_format == "json":
         return parse_dynamic_config_json(peer_id, payload)
-    elif payload_format == 'xml':
+    elif payload_format == "xml":
         return parse_dynamic_config_xml(peer_id, payload)
     else:
         raise ValueError("Invalid format: " + payload_format)
 
 
-def save_config(peers: List[Peer],
-                filename: str,
-                output_format: str = 'nsd',
-                use_zonefiles: bool = False,
-                force_output: bool = False,
-                diff: bool = False) -> bool:
+def save_config(
+    peers: List[Peer],
+    filename: str,
+    output_format: str = "nsd",
+    use_zonefiles: bool = False,
+    force_output: bool = False,
+    diff: bool = False,
+) -> bool:
     config_output = io.StringIO()
     with redirect_stdout(config_output):
-        if output_format == 'nsd':
+        if output_format == "nsd":
             generate_nsd(peers, use_zonefiles)
         else:
             raise ValueError("Invalid output format")
-    output_file, output_path = mkstemp(prefix='conf.', suffix='.tmp', dir='.')
+    output_file, output_path = mkstemp(prefix="conf.", suffix=".tmp", dir=".")
     os.write(output_file, config_output.getvalue().encode())
     os.close(output_file)
     os.chmod(output_path, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
 
     args = ["diff", "-u", filename, output_path]
-    logging.debug("Running diff: %s", ' '.join(args))
+    logging.debug("Running diff: %s", " ".join(args))
     res = subprocess.run(args, stdout=subprocess.PIPE, text=True)
     logging.debug("diff returned %d", res.returncode)
     if res.returncode == 0 and not force_output:
@@ -242,25 +262,24 @@ def save_config(peers: List[Peer],
 
 def main() -> None:
     """Command line tool"""
-    parser = argparse.ArgumentParser(description='DPER')
+    parser = argparse.ArgumentParser(description="DPER")
 
-    parser.add_argument('--config',
-                        dest='config',
-                        default='dper.yaml',
-                        metavar='filename',
-                        help='Configuraton file')
-    parser.add_argument('--offline',
-                        dest='offline',
-                        action='store_true',
-                        help="Offline (force cache)")
-    parser.add_argument('--force',
-                        dest='force',
-                        action='store_true',
-                        help="Force update")
-    parser.add_argument('--debug',
-                        dest='debug',
-                        action='store_true',
-                        help="Enable debugging")
+    parser.add_argument(
+        "--config",
+        dest="config",
+        default="dper.yaml",
+        metavar="filename",
+        help="Configuraton file",
+    )
+    parser.add_argument(
+        "--offline", dest="offline", action="store_true", help="Offline (force cache)"
+    )
+    parser.add_argument(
+        "--force", dest="force", action="store_true", help="Force update"
+    )
+    parser.add_argument(
+        "--debug", dest="debug", action="store_true", help="Enable debugging"
+    )
 
     args = parser.parse_args()
 
@@ -272,26 +291,32 @@ def main() -> None:
     voluptuous.humanize.validate_with_humanized_errors(config, CONFIG_SCHEMA)
 
     peers = []
-    for peer_id, peer_config in config.get('peers', {}).items():
-        ext = peer_config['format']
-        if config.get('cache_dir') is not None:
-            cache = os.path.join(config.get('cache_dir'), f'{peer_id}.{ext}')
+    for peer_id, peer_config in config.get("peers", {}).items():
+        ext = peer_config["format"]
+        if config.get("cache_dir") is not None:
+            cache = os.path.join(config.get("cache_dir"), f"{peer_id}.{ext}")
         else:
             cache = None
-        peers.extend(process_dper(peer_id=peer_id,
-                                  source=peer_config['source'],
-                                  cache=cache,
-                                  force_cache=args.offline,
-                                  payload_format=peer_config['format']))
+        peers.extend(
+            process_dper(
+                peer_id=peer_id,
+                source=peer_config["source"],
+                cache=cache,
+                force_cache=args.offline,
+                payload_format=peer_config["format"],
+            )
+        )
 
     check_peers(peers)
-    changed = save_config(peers=peers,
-                          filename=config['output_file'],
-                          use_zonefiles=config.get('zonefiles', True),
-                          force_output=args.force,
-                          diff=config.get('output_diff', False))
+    changed = save_config(
+        peers=peers,
+        filename=config["output_file"],
+        use_zonefiles=config.get("zonefiles", True),
+        force_output=args.force,
+        diff=config.get("output_diff", False),
+    )
 
-    reconfigure = config.get('reconfigure_command')
+    reconfigure = config.get("reconfigure_command")
     if changed and reconfigure:
         logging.info("reconfiguring using %s", reconfigure)
         args = shlex.split(reconfigure)
